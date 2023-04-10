1645c10b44b2a7f3809f053a92a695ae829de7af
vergtx <vergtx@naver.com>
2023-04-10 14:45:32 +0900

    vv2

---
+++ javascript/13.js
@@ -0,0 +1,11 @@
+//function show() {
+//펑션사용해서 알람 띄우기alert("안녕하세요") ;
+
+//id가 뭐인것을 찾아올께 
+//
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("h2id").innerHTML = "안녕하세요" ;
+});
+ //dom 이 만들어지고 나서 펑션 누르면 h2id에 어떤 내용 띄울껀지
